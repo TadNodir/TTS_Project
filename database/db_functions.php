f677fb39b9c4d1925d9327fde02efaffe9fde870
Ivan Volkov <volkv.iav@gmail.com>
2022-12-18 14:40:15 +0100

    Darstellungen aus der Datenbank Durchblättern

--- database/db_functions.php
+++ database/db_functions.php
@@ -1,7 +1,7 @@
 <?php
 function createLink($user = "dev_tts")
 {
-    $servername = "127.0.0.1";
+    $servername = "localhost";
     $username = $user;
     $password = "QN7ZAqgGY9wZ";
     $database = "swe_tts";
@@ -29,4 +29,58 @@ function closeLink($link, $result = null)
         mysqli_free_result($result);
     }
     mysqli_close($link);
+}
+
+function db_select_anst_spiele($link, $userid, $page_anst, $num){
+    $query = "SELECT
+    spiele.id AS SPIEL,
+    t.id, t.land AS LAND1,
+    t2.id, t2.land AS LAND2,
+    tore_team1, tore_team2,
+    uhrzeit,
+
+    (SELECT tipp_team1
+     FROM tipps WHERE spiele.id = id AND tipper = '$userid') AS TIPP1,
+    (SELECT tipp_team2
+     FROM tipps WHERE spiele.id = id AND tipper = '$userid') AS TIPP2,
+    (SELECT verdient
+     FROM tipps WHERE spiele.id = id AND tipper = '$userid') AS VERDIENT
+FROM spiele
+         JOIN teams t on spiele.team_1 = t.id
+         JOIN teams t2 on spiele.team_2 = t2.id
+WHERE beendet = 0 
+LIMIT $page_anst, $num";
+    return mysqli_query($link, $query);
+}
+
+function db_select_verg_spiele($link ,$userid, $page_verg,$num){
+    $query = "SELECT
+    spiele.id AS SPIEL,
+    t.id, t.land AS LAND1,
+    t2.id, t2.land AS LAND2,
+    tore_team1, tore_team2,
+    
+
+    (SELECT tipp_team1
+     FROM tipps WHERE spiele.id = id AND tipper = '$userid') AS TIPP1,
+    (SELECT tipp_team2
+     FROM tipps WHERE spiele.id = id AND tipper = '$userid') AS TIPP2,
+    (SELECT verdient
+     FROM tipps WHERE spiele.id = id AND tipper = '$userid') AS VERDIENT
+FROM spiele
+         JOIN teams t on spiele.team_1 = t.id
+         JOIN teams t2 on spiele.team_2 = t2.id
+WHERE beendet = 1
+LIMIT $page_verg,$num";
+    return mysqli_query($link, $query);
+}
+
+function db_scoreboard_punkte($link, $page_scr, $num){
+    $query = "SELECT nickname, punktestand FROM benutzer WHERE rolle = 0 ORDER BY punktestand DESC LIMIT $page_scr,$num";
+    return mysqli_query($link, $query);
+}
+
+function db_scoreboard_ergebniss($link, $userid){
+    $query = "SELECT punktestand FROM benutzer WHERE id = '$userid'";
+    return mysqli_query($link, $query);
 }
